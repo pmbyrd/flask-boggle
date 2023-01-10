82916cc00cbb6fb9a5ce030ee5594270feb00d3a
Perriesha <pmbyrd@gmail.com>
2023-01-09 21:47:29 -0800

    added  score function

--- static/boggle.js
+++ static/boggle.js
@@ -1,5 +1,6 @@
 // *remember when using jquery, you need to use $() to select the element, and working with classes is helpful
 const form = document.querySelector('form');
+let score = 0;
 
 form.addEventListener("submit", async function(e){
     e.preventDefault();
@@ -13,7 +14,12 @@ form.addEventListener("submit", async function(e){
     $(".guess").val("")
     if(response.data.result === "ok"){
         displayWord(word)
-    } 
+        score += word.length
+        displayScore(score)
+        displayMessage(`Added: ${word}`, "ok")
+    } else if (response.data.result === "not-on-board") {
+        displayMessage(`Not a valid word: ${word}`, "err")
+    }
 })
 
     // todo displayWord(word)
@@ -23,21 +29,40 @@ function displayWord(word) {
     $ul.append($li);
 }
     
-
     // Todo displayScore()
-function displayScore(word) {
+function displayScore() {
     let $score = $(".points");
-    let score = word.length;
     $score.text(score);
 }
-   
     
     // todo displayMessage(msg, cls) displayes the JSON response
-    
+function displayMessage(msg, cls) {
+    let $msg = $(".msg");
+    $msg.text(msg).removeClass().addClass(`msg ${cls}`);
+}    
 
     // Todo displayTimer()
+function displayTimer() {
+    let $timer = $(".timer");
+    let timer = 60;
+    $timer.text(timer);
+}
 
-    // Todo make countdown()
+// Todo make countdown()
+// function countdown() {
+//     let timer = setInterval(function() {
+//         time--;
+//         $(".timer").text(time);
+//         if (time === 0) {
+//             clearInterval(timer);
+//             // Todo change this to a gameover() function for posting to the backend
+//             $(".guess").attr("disabled", true);
+//             $(".guess-btn").attr("disabled", true);
+//         }
+//     }
+// }
+
+
     // *this will be called every second to update the timer
     // will need to be async , when the timer reaches 0, it will need to make a request to the server to get the score
     // todo postScore()
